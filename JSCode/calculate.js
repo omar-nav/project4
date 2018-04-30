b1b2670ccdd1b02c182cf3321fe81d39587b90bf
primaryuser <13755487+omar-nav@users.noreply.github.com>
2018-04-30 16:19:09 -0500

    created calculate.js for post-tax totals

---
+++ JSCode/calculate.js
@@ -0,0 +1,18 @@
+function calculate() {
+    if (isNaN(document.forms[0].menuItem1.value) || isNaN(document.forms[0].menuItem2.value) || isNaN(document.forms[0].menuItem3.value) || isNaN(document.forms[0].menuItem4.value) || isNaN(document.forms[0].menuItem5.value)) {
+        alert("Numeric values are required when ordering from the menu");
+        error = true;
+    } else {
+            error=false;
+            var menuItemOne = parseInt(document.forms[0].menuItem1.value);
+            var menuItemTwo = parseInt(document.forms[0].menuItem2.value);
+            var menuItemThree = parseInt(document.forms[0].menuItem3.value);
+            var menuItemFour = parseInt(document.forms[0].menuItem4.value);
+            var menuItemFive = parseInt(document.forms[0].menuItem5.value);
+        } if (!error) {
+            var total = (menuItemOne * 5) + (menuItemTwo * 7)+ (menuItemThree) + (menuItemFour * 4)+ (menuItemFive * 2);
+            var tax = (0.0825);
+            var postTaxTotal = total * (1+tax);
+            window.alert("Your order totals: $" + postTaxTotal.toFixed(2));
+            }
+}
